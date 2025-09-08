b69ff7c26e27c9a8329383eb79bba9fa39f57070
iamlouxu <wayne199939@gmail.com>
2025-09-08 15:03:02 +0800

    feature - counter practice & data components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
-function App() {}
+import { useState } from "react";
+import { Return } from "./components/data.js";
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <span>目前數字：{count}</span>
+      <button onClick={() => setCount(Return(count))}>-1</button>
+    </>
+  );
+}
 
 export default App;
