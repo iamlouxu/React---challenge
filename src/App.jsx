5082e5f39ba0fea0133f3759d97bc4409170db65
iamlouxu <wayne199939@gmail.com>
2025-09-09 17:35:39 +0800

    feature - delete button

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
 import BlogList from "./components/BlogList";
-
 function App() {
-  const [blogs, Setblogs] = useState([
+  const [blogs, setBlogs] = useState([
     { name: "wayne", age: 20, id: 1 },
     { name: "louxu", age: 23, id: 2 },
     { name: "gay", age: 18, id: 3 },
     { name: "pussy", age: 27, id: 4 },
   ]);
 
+  function handleList(id) {
+    const newBlog = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlog);
+  }
+
   return (
     <>
-      <BlogList blogs={blogs} name="我要當軟體工程師" />
+      <BlogList blogs={blogs} name="老徐是軟體工程師" handleList={handleList} />
     </>
   );
 }
--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,11 +1,12 @@
-function BlogList({ blogs, name }) {
+function BlogList({ blogs, name, handleList }) {
   return (
     <>
       <h1>{name}</h1>
       {blogs.map((blog) => (
         <div key={blog.id}>
-          <h2>{blog.name}</h2>
-          <p>{blog.age}</p>
+          <h1>{blog.name}</h1>
+          <p>{blog.age}歲</p>
+          <button onClick={() => handleList(blog.id)}>刪除</button>
         </div>
       ))}
     </>
